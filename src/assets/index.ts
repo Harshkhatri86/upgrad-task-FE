27cf3cd3f44f91f23f29a2802d3627eee0248e90
harsh khatri <khatriharsh86@gmail.com>
2024-06-09 20:22:40 +0530

    Added icon

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import Tv from './tv.png'
+import luggage from './luggage.png'
+import group from './group.png'
+import book from './book.png'
+import beaker from './beaker.png'
+
+export {Tv , luggage ,group , book , beaker}
--- src/component/ImageAnimation/Index.tsx
+++ src/component/ImageAnimation/Index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import diamond from "../../assets/diamond.png";
+import { Tv, luggage, group, book, beaker } from "../../assets/index";
 
 function ImageAnimation() {
   return (
     <>
       <div className="flex p-2 h-12 z-10 bg-white rounded-full  border-borderGreen items-center border-2 absolute   animate-fadeInOutLeft top-4 left-2">
-        <div className="rounded-128 flex items-center justify-center ">
-          <i className="icon-stand text-xl text-mono-white"></i>
+        <div className="rounded-128 p-1 flex items-center justify-center bg-borderGreen rounded-full">
+          <img src={Tv} alt="" className="w-5" />
         </div>
-        <div className="flex flex-col flex-1">
+        <div className="flex p-3 flex-col flex-1">
           <div className="text-xs leading-4.5 text-neutral-800 font-semibold">
             Guaranteed-to-Run Sessions
           </div>
@@ -15,10 +17,10 @@ function ImageAnimation() {
       </div>
 
       <div className="flex p-2 h-12 z-10 rounded-full bg-white  border-borderGreen items-center border-2 absolute animate-fadeInOutRight2 top-12 right-9">
-        <div className="rounded-128 flex items-center justify-center ">
-          <i className="icon-diamond-underline text-xl text-mono-white"></i>
+        <div className="rounded-128 p-1 rounded-full flex items-center justify-center bg-borderGreen">
+          <img src={diamond} alt="" className="w-5" />
         </div>
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col flex-1 pl-3">
           <div className="text-xs leading-4.5 text-neutral-800 font-semibold">
             2-Year Scrum Alliance Membership
           </div>
@@ -27,8 +29,8 @@ function ImageAnimation() {
       </div>
 
       <div className="flex p-2 h-12 z-10 rounded-full bg-white  border-borderGreen items-center rounded-128 border-2 absolute animate-fadeInOutLeft2 top-36 left-4">
-        <div className="rounded-128 flex items-center justify-center ">
-          <i className="icon-luggage text-xl text-mono-white"></i>
+        <div className="rounded-128 p-1 flex items-center justify-center bg-borderGreen rounded-full">
+          <img src={luggage} alt="" className="w-5" />
         </div>
         <div className="flex flex-col flex-1">
           <div className="text-xs leading-4.5 text-neutral-800 font-semibold">
@@ -39,11 +41,11 @@ function ImageAnimation() {
       </div>
 
       <div className="flex p-2 h-12 rounded-full bg-white border-borderGreen items-center border-2 absolute animate-fadeInOutRight top-53 right-4">
-        <div className="flex items-center justify-center ">
-          <i className="icon-users text-xl text-mono-white "></i>
+        <div className="rounded-128 p-1 flex items-center justify-center bg-borderGreen rounded-full">
+          <img src={group} alt="" className="w-5" />
         </div>
-        <div className="flex flex-col flex-1">
-          <div className="text-xs leading-4.5 text-neutral-800 font-semibold">
+        <div className="flex flex-col p-3 flex-1">
+          <div className="text-xs  leading-4.5 text-neutral-800 font-semibold">
             Elite Panel of CSTs
           </div>
           <div className="text-sm leading-5.5 text-neutral-700 font-bold"></div>
@@ -51,10 +53,10 @@ function ImageAnimation() {
       </div>
 
       <div className="flex p-2 h-12 rounded-full bg-white border-borderGreen items-center border-2 absolute animate-fadeInOutRight2 bottom-15 left-30">
-        <div className="rounded-128 flex items-center justify-center ">
-          <i className="icon-book-closed text-xl text-mono-white"></i>
+        <div className="rounded-128 p-1 flex items-center justify-center bg-borderGreen rounded-full">
+          <img src={book} alt="" className="w-5" />
         </div>
-        <div className="flex flex-col flex-1">
+        <div className="flex p-3 flex-col flex-1">
           <div className="text-xs leading-4.5 text-neutral-800 font-semibold">
             Complimentary Courses Worth $300
           </div>
@@ -63,10 +65,10 @@ function ImageAnimation() {
       </div>
 
       <div className="flex p-2 h-12 rounded-full border-borderGreen bg-white  z-10 items-center border-2 absolute animate-fadeInOutLeft bottom-6 left-7">
-        <div className="rounded-128 flex items-center justify-center ">
-          <i className="icon-beaker text-xl text-mono-white"></i>
+        <div className="rounded-128 p-1 flex items-center justify-center bg-borderGreen rounded-full">
+          <img src={beaker} alt="" className="w-5" />
         </div>
-        <div className="flex flex-col flex-1">
+        <div className="flex p-3 flex-col flex-1">
           <div className="text-xs leading-4.5 text-neutral-800 font-semibold">
             Experiential Learning
           </div>
